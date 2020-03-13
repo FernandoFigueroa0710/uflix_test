0619de1e2070120096f14994c46e297f47d35b38
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-03-12 18:34:36 -0600

    folder rearange

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./main.styles";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
