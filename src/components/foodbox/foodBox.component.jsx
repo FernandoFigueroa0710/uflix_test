8f7a18155af316f99d10fb9017c211c3c2ee46a2
Fernando Figueroa <fernandoffigueroa@gmail.com>
2020-03-14 22:32:11 -0600

    FOOD BOX ITEMS Mapping items from resources

--- src/components/foodbox/foodBox.component.jsx
+++ src/components/foodbox/foodBox.component.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import FoodBoxItem from "./foodBoxItem.component";
-
-const Foodbox = () => (
-	<div>
-		<FoodBoxItem />
-	</div>
-);
+import myFoods from "../../resources/foods";
+const Foodbox = () => {
+	return (
+		<div className="foodBox-container">
+			<div className="foodBox-items">
+				{myFoods
+					? myFoods.map((item, i) => (
+							<FoodBoxItem key={i} item={item} />
+					  ))
+					: "NOT"}
+			</div>
+			<div className="foodBox-info">
+				<span>Food INFO </span>
+			</div>
+		</div>
+	);
+};
 
 export default Foodbox;
